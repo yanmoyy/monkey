4f82dde614ea7169278fbe5403d4034f91b092a1
yanmo <67538999+yanmoyy@users.noreply.github.com>
2025-08-11 14:02:58 +0900

    Data Types (string)

--- token/token.go
+++ token/token.go
@@ -5,8 +5,9 @@ const (
 	EOF     = "EOF"
 
 	// Identifiers + literals
-	IDENT = "IDENT" // add, foobar, x, y, ...
-	INT   = "INT"   // 123456
+	IDENT  = "IDENT"  // add, foobar, x, y, ...
+	INT    = "INT"    // 123456
+	STRING = "STRING" // "foobar"
 
 	// Operators
 	ASSIGN   = "="
