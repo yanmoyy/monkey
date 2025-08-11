6fd7dd11a7a4d54939565ebb3754494f00ef8269
yanmo <67538999+yanmoyy@users.noreply.github.com>
2025-08-11 14:47:32 +0900

    builtins (len)

---
+++ evaluator/builtins.go
@@ -0,0 +1,22 @@
+package evaluator
+
+import "github.com/yanmoyy/go-interpreter/object"
+
+var builtins = map[string]*object.Builtin{
+	"len": {
+		Fn: func(args ...object.Object) object.Object {
+			if len(args) != 1 {
+				return newError("wrong number of argument. got=%d, want=1",
+					len(args))
+			}
+
+			switch arg := args[0].(type) {
+			case *object.String:
+				return &object.Integer{Value: int64(len(arg.Value))}
+			default:
+				return newError("argument to `len` not supported, got %s",
+					args[0].Type())
+			}
+		},
+	},
+}
