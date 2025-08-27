f38eb9e8c9c871378f8ae87f32385bca4f53a629
yanmo <67538999+yanmoyy@users.noreply.github.com>
2025-08-27 10:38:31 +0900

    local bindings

--- object/object.go
+++ object/object.go
@@ -187,6 +187,7 @@ type Hashable interface {
 
 type CompiledFunction struct {
 	Instructions code.Instructions
+	NumLocals    int
 }
 
 func (cf *CompiledFunction) Type() ObjectType { return COMPILED_FUNCTION_OBJ }
